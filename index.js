dfc2071130569e1d481d8fe11d4b8342e8e7a496
Israel Gonzalez <israelgonzalez@lacroizrules.lan>
2019-07-24 20:21:22 -0700

    Core POST, GETs, and DELETE functions implemented. PUT in progress

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
 // implement your API here
+const express = require("express");
+
+const server = express();
+
+const Users = require("./data/db.js");
+
+server.use(express.json());
+
+server.post("/api/users", (req, res) => {
+  const userInfo = req.body;
+  //console.log("Request body", req.body);
+  Users.insert(userInfo)
+    .then(user => {
+      userInfo.name && userInfo.bio
+        ? res.status(201).json(user)
+        : res.status(400).json({
+            errorMessage: "Please provide name and bio for the user."
+          });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "There was an error while saving the user to the database"
+      });
+    });
+});
+
+server.get("/api/users", (req, res) => {
+  Users.find()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The users information could not be retrieved."
+      });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  Users.findById(id)
+    .then(user => {
+      user
+        ? res.status(200).json(user)
+        : res.status(404).json({
+            message: "The user with the specified ID does not exist."
+          });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The user information could not be retrieved." });
+    });
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  Users.remove(id)
+    .then(user => {
+      user
+        ? res.status(204).end()
+        : res.status(404).json({
+            message: "The user with the specified ID does not exist."
+          });
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The user could not be removed" });
+    });
+});
+
+server.listen(4000, () => console.log("API running on port 4000"));
